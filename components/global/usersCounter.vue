86c271b60c8acb3af323a55acd5f4163676d4082
sanamiy <sanamissile1@gmail.com>
2022-01-27 14:22:13 +0900

    counter function 完成

---
+++ components/global/usersCounter.vue
@@ -0,0 +1,11 @@
+<template>
+  
+</template>
+<script>
+export default {
+async beforeMount(){
+const userRef = this.$fire.firestore.collection('users').doc(user.uid)
+      if (userRef == null){ return }
+}
+}
+</script>
--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 admin.initializeApp();
 const db = admin.firestore()
+const increment = admin.firestore.FieldValue.increment(1)
 
 // usersの数を数えたい
 
@@ -9,15 +10,15 @@ const db = admin.firestore()
 exports.incrementUser = functions.firestore
     .document('users/{userId}')
     .onCreate((snap, context) => {
-      const FieldValue = admin.firestore.FieldValue;
-      db.doc("usersData/doc").update({usersCount: FieldValue.increment(1)});
-      return;
+      db.doc("usersData/doc").update({usersCount: increment});
+      return null;
 });
 
 // 毎日1回usersの数をカウントする
-exports.scheduledCountUser = functions.pubsub.schedule('every day').onRun((context) => {
+exports.scheduledCountUser = functions.pubsub.schedule('every 24 hours').onRun((context) => {
   db.collection("users").get().then(snap => {
       db.doc("usersData/doc").update({usersCount: snap.size })
+      functions.logger.log("number of users: " ,snap.size);
   });
-  return;
+  return null;
 });
