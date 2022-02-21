deebe9864ef4ddd6b6545e498056a7db10d78cea
sanamiy <sanamissile1@gmail.com>
2022-02-22 03:38:27 +0900

    Update signUp.vue
    これでおおよそ動くはずだが、
    TypeError: Cannot read properties of undefined (reading 'EMAIL_PASSWORD_SIGN_IN_METHOD')
        at eval　とエラーが出る

--- components/global/signUp.vue
+++ components/global/signUp.vue
@@ -22,49 +22,53 @@ export default {
   data: () => ({
     email: "",
     randomPassword:"",
-    newEmail: true
+    isNewAccount: ""
   }),
   methods: {
+    async isEmailRegistered(email){
+      const signInMethods = await this.$fire.auth.fetchSignInMethodsForEmail(email)
+      return signInMethods.indexOf(this.$fire.auth.EmailAuthProvider.EMAIL_PASSWORD_SIGN_IN_METHOD) == -1
+    },
     async createUser() {
-      const continueUrl = process.env.baseUrl + `/signature-edit/?email=${this.email}`
+      const continueUrl = process.env.baseUrl + `/signature-edit`
       const actionCodeSettings = {
         url: continueUrl,
         handleCodeInApp: true
       }
-      this.$toast.info("少々お待ちください")
-
-      //アカウントが既に存在するかどうか確認する
-      const providers = await this.$fire.auth.fetchSignInMethodsForEmail(this.email);
-      if (providers.findIndex(p => p === this.$fire.auth.EmailAuthProvider.EMAIL_PASSWORD_SIGN_IN_METHOD) !== -1) {
-        this.$toast.info("既に登録されたメールアドレスです");
-        newEmail = false
-        return;
-      } 
-       // まだアカウントが存在していない場合アカウントを作成する
-      this.randomPassword = Math.random().toString(30).slice(2)
-      this.$fire.auth.createUserWithEmailAndPassword(
-        this.email,
-        this.randomPassword
-        // ユーザー名を設定する
-        ).then((user)=> {
-          user.updateProfile({
-            displayName: "署名にご協力くださる皆"
+      // アカウントが作成済かを調べる
+      this.$fire.auth.fetchSignInMethodsForEmail(this.email)
+       .then((signInMethods)=>{
+         console.log(this.$fire.auth.EmailAuthProvider.EMAIL_PASSWORD_SIGN_IN_METHOD)
+          if ( signInMethods.indexOf(this.$fire.auth.EmailAuthProvider.EMAIL_PASSWORD_SIGN_IN_METHOD) == -1){
+            this.isNewAccount = true
+          } else {
+            this.isNewAccount =false
+          }
+       }).then(()=>{
+         //アカウントが作成済みの場合
+          if (this.isNewAccount == true ){
+            this.$toast.info("アカウントを新規作成します。少々お待ち下さい")
+            this.randomPassword = Math.random().toString(30).slice(2)
+            this.$fire.auth.createUserWithEmailAndPassword(
+                this.email,
+                this.randomPassword
+                // ユーザー名を設定する
+            ).then((user)=> {
+              user.updateProfile({ displayName: "署名にご協力くださる皆" })
+            }).catch((error)=>{
+              this.$toast.error(error)
+            })
+       }}).then(()=>{
+         // アカウントを作成ののち、またはアカウントが存在していた場合はそのまま、サインインリンクを送信する
+         this.$fire.auth.sendSignInLinkToEmail(this.email, actionCodeSettings)
+       }).then((res)=>{
+            this.$toast.success('メールを送信しました。メール内のリンクからログインしてください')
+            this.$router.push(`/signature?accountCreated=1`)
+       }).catch((error)=>{
+            console.log(error)
+            this.$toast.error(error.message)
           })
-        }).catch((error)=>{
-          this.$toast.error(error)
-        })
-      // 存在していた場合はそのままサインインリンクを送信する
-      this.$fire.auth.sendSignInLinkToEmail(this.email, actionCodeSettings)
-      .then((res)=>{
-          this.$toast.success('メールを送信しました。メール内のリンクからログインしてください')
-          this.$router.push(`/signature?accountCreated=1`)
-      })
-      .catch(( )=>{
-        console.log(error)
-        this.$toast.error(error.message)
-      })
-    }
-  }
+    }}
 }
 
 </script>
